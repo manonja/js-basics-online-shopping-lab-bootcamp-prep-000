279b7a885c00c79d03a9f536c44edcd7715736f1
manon jacquin <manon.jacquin@gmail.com>
2018-08-30 16:09:22 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ function removeFromCart(item) {
   }
   
 function placeOrder(cardNumber) {
-  if (cardNumber.length === 0){
+  if (cardNumber ){
     return `Sorry, we don't have a credit card on file for you.`
   } 
   else {
